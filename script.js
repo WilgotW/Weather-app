2e55fe55152934ebe5c2f6f3c9fb6787fb448ce6
Wilgot Wardell <21wiwa@ad.ssis.nu>
2022-07-25 21:50:27 +0200

    search working

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const button = document.getElementById("search-button");
 const inputSearch = document.getElementById("search-input");
+const countryLocation = document.getElementById("country-title");
+const weather = document.getElementById("data");
+const description = document.getElementById("description");
 
 button.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputSearch.value +'&appid={cf5b6acb44941cb461975c7ad6ac6d50}')
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputSearch.value +'&appid=cf5b6acb44941cb461975c7ad6ac6d50')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            var nameValue = data['name'];
+            var temp = data['main']['temp'];
+            var descValue = data['weather'][0]['description'];
+
+            countryLocation.innerHTML = nameValue;
+            weather.innerHTML = temp;
+            description.innerHTML = descValue;
+        })
 
     .catch(err => alert("Wrong city name"));
-
 });
 
 
