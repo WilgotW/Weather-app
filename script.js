841faa7c8a2d0f6e9e275c712cf080285953d451
Wilgot Wardell <21wiwa@ad.ssis.nu>
2022-07-25 22:05:51 +0200

    conversion and bug fixes

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ const weather = document.getElementById("data");
 const description = document.getElementById("description");
 
 button.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputSearch.value +'&appid=cf5b6acb44941cb461975c7ad6ac6d50')
-        .then(response => response.json())
-        .then(data => {
-            var nameValue = data['name'];
-            var temp = data['main']['temp'];
-            var descValue = data['weather'][0]['description'];
-
-            countryLocation.innerHTML = nameValue;
-            weather.innerHTML = temp;
-            description.innerHTML = descValue;
-        })
-
-    .catch(err => alert("Wrong city name"));
+    call();
 });
 
+function call(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputSearch.value +'&appid=cf5b6acb44941cb461975c7ad6ac6d50')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var temp = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
 
+        countryLocation.innerHTML = nameValue;
+        weather.innerHTML = convertToCelcius(temp) + "\u00B0C";
+        description.innerHTML = descValue;
+    })
+
+    .catch(err => alert("Wrong city name"));
+}
+inputSearch.value = 'Stockholm';
+call();
+
+let convertToCelcius = (kelvin) => Math.floor(kelvin - 273.15);
